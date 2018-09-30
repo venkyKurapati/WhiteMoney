e68493aa3961ef615de139e189fa44c498fd8bef
Venkatesh <venkyinios@gmail.com>
2018-09-30 21:59:07 +0530

    initial commit

---
+++ WhiteMOney/WhiteMOneyTests/WhiteMOneyTests.swift
@@ -0,0 +1,36 @@
+//
+//  WhiteMOneyTests.swift
+//  WhiteMOneyTests
+//
+//  Created by Venkatesh on 30/09/18.
+//  Copyright © 2018 Venkatesh. All rights reserved.
+//
+
+import XCTest
+@testable import WhiteMOney
+
+class WhiteMOneyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
